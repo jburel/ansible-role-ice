2d1b8960328a0c97bb17b46ed811155c1bae3e0c
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-11-23 21:38:54 +0000

    Remove unused variable

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,18 +5,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_ice_version(Command, TestinfraBackend):
-    host = TestinfraBackend.get_hostname()
     assert Command.exists('icegridnode')
     c = Command('icegridnode --version')
     assert c.stderr.startswith('3.6.')
 
 
 def test_icepy_version(Command, TestinfraBackend):
-    host = TestinfraBackend.get_hostname()
     c = Command('python -c "import Ice; print Ice.stringVersion()"')
     assert c.stdout.startswith('3.6.')
 
 
 def test_ice_devel(Package, TestinfraBackend):
-    host = TestinfraBackend.get_hostname()
     assert Package('ice-all-devel').is_installed
